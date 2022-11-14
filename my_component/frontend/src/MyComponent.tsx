e26c4987fbfe708ce4935969e30830c931bf33ae
Kota Mori <kmori05@gmail.com>
2022-11-14 20:10:57 +0900

    streamlit-custom-component-experiment

---
+++ my_component/frontend/src/MyComponent.tsx
@@ -0,0 +1,112 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
+
+interface State {
+  numClicks: number
+  isFocused: boolean
+}
+
+/**
+ * This is a React-based component template. The `render()` function is called
+ * automatically when your component should be re-rendered.
+ */
+class MyComponent extends StreamlitComponentBase<State> {
+  public state = { numClicks: 0, isFocused: false }
+  constructor(prop: any) {
+    super(prop)
+    console.log("constructor!")
+  }
+  public componentDidMount() { 
+    console.log("Component did mount!");
+    Streamlit.setFrameHeight(); 
+  }
+  public componentDidUpdate() {
+     console.log(String(this.state.numClicks), "Component did update!");
+     const x = document.getElementById("hoge")
+     if (x != null) { x.innerHTML = String(this.state.numClicks) }
+     Streamlit.setFrameHeight();
+  }
+  public componentWillUnmount() {
+    console.log("Component will mount!"); 
+    Streamlit.setFrameHeight(); 
+  }
+  
+  public render = (): ReactNode => {
+    console.log("Render!");
+    
+    // Arguments that are passed to the plugin in Python are accessible
+    // via `this.props.args`. Here, we access the "name" arg.
+    const name = this.props.args["name"]
+
+    // Streamlit sends us a theme object via props that we can use to ensure
+    // that our component has visuals that match the active theme in a
+    // streamlit app.
+    const { theme } = this.props
+    const style: React.CSSProperties = {}
+
+    // Maintain compatibility with older versions of Streamlit that don't send
+    // a theme object.
+    if (theme) {
+      // Use the theme object to style our button border. Alternatively, the
+      // theme style is defined in CSS vars.
+      const borderStyling = `1px solid ${
+        this.state.isFocused ? theme.primaryColor : "gray"
+      }`
+      style.border = borderStyling
+      style.outline = borderStyling
+    }
+
+    // Show a button and some text.
+    // When the button is clicked, we'll increment our "numClicks" state
+    // variable, and send its new value back to Streamlit, where it'll
+    // be available to the Python program.
+    return (
+      <span>
+        Hello, {name}! &nbsp;
+        <br />
+        N. clicks update by javascript = <span id="hoge"></span>
+        <br />
+        <button
+          style={style}
+          onClick={this.onClicked}
+          disabled={this.props.disabled}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+        >
+          Click Me!
+        </button>
+      </span>
+    )
+  }
+
+  /** Click handler for our "Click Me!" button. */
+  private onClicked = (): void => {
+    // Increment state.numClicks, and pass the new value back to
+    // Streamlit via `Streamlit.setComponentValue`.
+    this.setState(
+      prevState => ({ numClicks: prevState.numClicks + 1 }),
+      () => Streamlit.setComponentValue(this.state.numClicks)
+    )
+  }
+
+  /** Focus handler for our "Click Me!" button. */
+  private _onFocus = (): void => {
+    this.setState({ isFocused: true })
+  }
+
+  /** Blur handler for our "Click Me!" button. */
+  private _onBlur = (): void => {
+    this.setState({ isFocused: false })
+  }
+}
+
+// "withStreamlitConnection" is a wrapper function. It bootstraps the
+// connection between your component and the Streamlit app, and handles
+// passing arguments from Python -> Component.
+//
+// You don't need to edit withStreamlitConnection (but you're welcome to!).
+export default withStreamlitConnection(MyComponent)
